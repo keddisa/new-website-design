a37a733568dbdecf615d8ec5bfbed3a1415303ea
keddisa <keddisa@gmail.com>
2020-06-15 23:39:28 -0400

    refactor testimonials

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { TestimonialCard } from "../components"
+import { TestimonialCard, TitleAside } from "../components"
 
 const Testimonials = () => {
     return (<div className="testimonials">
+        <TitleAside titleOne="TESTIM"
+            titleTwo="ONIALS"
+            justify="left"/> 
+        <div>
         <TestimonialCard    headshotImage="headshot-temp.jpg"
                             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             name="John Doe"
@@ -15,6 +19,8 @@ const Testimonials = () => {
                             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             name="John Doe"
                             title="Head of Hr, The big Organization"/>
+        </div>
+
     </div>)
 }
 
