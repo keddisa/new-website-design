24550b3e30c3ac0959d32ae54b3543f2c239e0db
keddisa <keddisa@gmail.com>
2020-06-26 20:41:41 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89cfa0f74f3886a0af34.js"
+    "url": "webpack-runtime-18863c7f0abfd1957ef8.js"
   },
   {
     "url": "framework-8dcecaaefd71e2213eb2.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d49e124c58418db45beeb371977d54e9"
+    "revision": "55bd3776af357c8cb01989be60cb2862"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7a8784191e6a0b0b5c820ec9eda93a4e"
+    "revision": "f251a18dcd46082b73bf2b0fbfe15526"
   },
   {
     "url": "manifest.webmanifest",
