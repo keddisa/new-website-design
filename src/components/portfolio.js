ad7f13876594c34948cd1eeea84dc39c336f7527
keddisa <keddisa@gmail.com>
2020-06-28 12:34:34 -0400

    add additional technologies

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,23 +7,23 @@ const Portfolio = () => {
         {
             title: "Concentration Game",
             description: "Test your concentration. Two player game developed with React and React Hooks",
-            technologies: [Technologies.html, Technologies.css, Technologies.react],
+            technologies: [Technologies.html, Technologies.css, Technologies.javascript, Technologies.react],
             githubLink: "https://github.com/keddisa/concentration-game-react",
             liveLink: 'https://shinkei-suijaku.s3.us-east-2.amazonaws.com/index.html'
         },
         {
             title: "Concentration Game",
             description: "Test your concentration. Two player game developed with React and React Hooks",
-            technologies: [Technologies.html, Technologies.css, Technologies.react],
+            technologies: [Technologies.html, Technologies.css, Technologies.javascript, Technologies.react],
             githubLink: "https://github.com/keddisa/concentration-game-react",
             liveLink: 'https://shinkei-suijaku.s3.us-east-2.amazonaws.com/index.html'
         },
         {
-            title: "Concentration Game",
-            description: "Test your concentration. Two player game developed with React and React Hooks",
-            technologies: [Technologies.html, Technologies.css, Technologies.react],
-            githubLink: "https://github.com/keddisa/concentration-game-react",
-            liveLink: 'https://shinkei-suijaku.s3.us-east-2.amazonaws.com/index.html'
+            title: "The Simon Game",
+            description: "Test your memory with the Simon Game. One player game developed with Tailwind and jQuery",
+            technologies: [Technologies.html, Technologies.tailwind, Technologies.javascript, Technologies.jquery],
+            githubLink: "https://github.com/keddisa/the-simon-game",
+            liveLink: 'https://keddisa.github.io/the-simon-game/.'
         }
     ]
 
